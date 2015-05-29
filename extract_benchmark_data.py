5a2be1d41cbc636597b2058ecb7ace710b2d5646
Yin Cheng Ng <yincheng.ng@gmail.com>
2015-05-29 15:24:15 +0100

    Added new data simulations

--- extract_benchmark_data.py
+++ extract_benchmark_data.py
@@ -9,15 +9,21 @@ import scipy.io
 import numpy as np
 import pickle
 from copula_experiments import *
-
+'''
 data = scipy.io.loadmat('../data/benchmarks.mat')
 for key_array in data['benchmarks'][0]:
     key = str(key_array[0])
     print key
     x = data[key]['x'][0, 0]
     y = np.transpose(data[key]['t'][0,0])[0]
-    with open('./data/'+key+'.pickle', 'w') as f:
+    with open('./data/'+key+'.pickle', 'wb') as f:
         pickle.dump([x, y], f)
 
-with open('../data/simulated.pickle', 'w') as f:
+with open('../data/simulated.pickle', 'wb') as f:
     pickle.dump(confabulate_logit_data(d=20, N=520))
+'''
+with open('../data/simulated-d10.pickle', 'wb') as f:
+    pickle.dump(confabulate_logit_data(d=10, N=520))
+
+with open('../data/simulated-d5.pickle', 'wb') as f:
+    pickle.dump(confabulate_logit_data(d=5, N=520))
